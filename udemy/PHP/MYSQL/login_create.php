b2cf3df4a171bfb513269e6599e855aaf999545b
cyanobacteria <vodomelkor@gmail.com>
2017-10-26 11:35:05 +0800

    完成udemy/PHP/MYSQL/login_create.php 的 CreateUser function

--- udemy/PHP/MYSQL/login_create.php
+++ udemy/PHP/MYSQL/login_create.php
@@ -1,24 +1,34 @@
 <?php
-if(isset($_POST['submit'])){
-
-$username = $_POST['username'];
-$password = $_POST['password'];
-
-//echo $username;
-//echo $password;
-
-$connection = mysqli_connect('localhost','root','66','loginapp');
-
-if($connection){
-
-echo "connected!!";
-
-} else{
-die("filed");
-}
+CreateUser($_POST['username'],$_POST['password']);
 
 
 
+
+//CreateUser(名稱,密碼);
+function CreateUser($username,$password){
+    //確定資料有送出成功
+    if(isset($_POST['submit'])){
+    //$username = $_POST['username'];
+    //$password = $_POST['password'];
+    //連結資料庫
+    $connection = mysqli_connect('localhost','root','66','loginapp');
+        //確認資料庫連結成功或失敗
+        if($connection){
+            echo "connected!!";
+        } else{
+            die("filed");
+        }
+        //SQL語句
+        $query = "INSERT INTO users(username,password) ";
+        $query .= "VALUES ('$username', '$password') ";
+        //存入DB(這一句就會存入了)
+        $result = mysqli_query($connection, $query);
+        //確認存入為真
+        $result;
+        if(!$result){
+            die('Query FAILED!!' . mysqli_error());
+        }
+    }
 }
 
 
@@ -44,7 +54,7 @@ die("filed");
 <div class="container">
 
     <div class="col-sm-6">
-        <form action="login.php" method="post">
+        <form action="login_create.php" method="post">
             <div class="form-group">
                 <label for="username">Username</label>
                 <input type="text" name="username" class="form-control">
